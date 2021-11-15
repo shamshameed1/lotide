dbaf5f1abdbc3b27b8a7177b74e4daf8fab1331c
Shams Hameed <shamsbasim@yahoo.com>
2021-11-15 00:34:10 -0500

    without

--- without.js
+++ without.js
@@ -19,14 +19,13 @@ const assertArraysEqual = function(actual, expected) {
   };
 
   const without = function(source, itemsToRemove) {
-    let newArr = [];
+    let newArray = [];
     for (let index = 0; index < source.length; index++) {
-      //Is it in the array? yes or no? --> I don't want it in array, negate that statement
       if (!itemsToRemove.includes(source[index])) {
-        newArr.push(source[index]);
+        newArray.push(source[index]);
       } 
     }
-    return newArr;
+    return newArray;
   };
   
   //TEST 1 - ensure removal of items that are the same
@@ -37,3 +36,5 @@ const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
 // Make sure the original array was not altered by the without function
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
+// touch base later and retest yourself
