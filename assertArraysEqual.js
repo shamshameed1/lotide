53be6910b40bef160928e1c29707db7fddc2acc6
Shams Hameed <shamsbasim@yahoo.com>
2021-11-15 00:20:42 -0500

    assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+function eqArrays (ar1, ar2) {
+    if (ar1 === ar2) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+const assertArraysEqual = function(actual, expected) {
+  let arr1 = [];
+  let arr2 = [];
+  actual = eqArrays(arr1, arr2);
+  
+  if (actual === eqArrays(arr1, arr2)) {
+    console.log(`😄😄😄 Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== eqArrays(arr1, arr2)) {
+    console.log(`🥲🥲🥲 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+//TEST
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertArraysEqual(eqArrays(["1", "2", "3"], ["3", "2", "1"]), false);
