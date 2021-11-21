442d5d27be7adf42a4b25c760fd0ac023cbb7158
Shams Hameed <shamsbasim@yahoo.com>
2021-11-21 17:46:06 -0500

    findKey

---
+++ findKey.js
@@ -0,0 +1,53 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+        console.log(`😄😄 Assertion Passed: "${actual}" === "${expected}"`);
+        
+    }
+
+    else {
+        console.log(`🥲 🥲 Assertion Failed: "${actual}" !== "${expected}"`);
+        
+    }
+
+};
+
+const findKey = function(object, callback) {
+    for (let key in object) {
+      //console.log(key);
+      console.log(object[key]);
+      console.log(callback(object[key]));
+      if (callback(object[key])) {
+        return key;
+      } 
+    }
+    return undefined;
+  }
+  
+//   const callback = function(objKey) {
+//     let cbResult;
+//     if (objKey) {
+//       cbResult = true;
+//     } else {
+//       cbResult = false
+//     }
+//     return cbResult;
+//   }
+  
+  
+  console.log(findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 2)); // => "noma"
+
+  console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 5));
