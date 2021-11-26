2d398a361f249bcf936f8ca916f9159020952134
Shams Hameed <shamsbasim@yahoo.com>
2021-11-26 14:34:46 -0500

    map

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -33,7 +33,7 @@ const eqObjects = function(object1, object2) {
   };
 
   const assertObjectsEqual = function(actual, expected) {
-    const inspect = require('util').inspect; 
+    const inspect = require('util').inspect; // <= add this line
     if (eqObjects(actual, expected) === true) {
         console.log(`😄😄😄 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
       } else {
--- map.js
+++ map.js
@@ -1,26 +1,30 @@
 function eqArrays (ar1, ar2) {
-    if (ar1 === ar2) {
-        return true;
+  if (ar1.length !== ar2.length) {
+    return false;
+  }
+  for (let i = 0; i < ar1.length; i++) {
+    if (ar1[i] !== ar2[i]) {
+      return false;
     }
-    else {
-        return false;
-    }
-}
+  }
+  return true;
+};
 
 const assertArraysEqual = function(actual, expected) {
   let arr1 = [];
   let arr2 = [];
   actual = eqArrays(arr1, arr2);
   
+
   if (actual === eqArrays(arr1, arr2)) {
-    console.log(`😄😄😄 Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== eqArrays(arr1, arr2)) {
-    console.log(`🥲🥲🥲 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
-};
+}
 
 
-const words = ["ground", "control", "to", "major", "tom"];
+
 const map = function(array, callback) {
     const results = [];
     for(let item of array) {
@@ -29,36 +33,14 @@ const map = function(array, callback) {
     return results;
   }
 
-  const map2 = function(array, callback) {
-    const results2 = [];
-    for(let item of array) {
-        results2.push(callback(item));
-    }
-    return results2;
-  }
-  const firstLetter = function(item) {
-    item = [];
-    for (let letter of item) {
-      return letter[0];
-    }
-  }
 
-const getLength = function(item) {
-    return item.length;
-  }
+ 
+  const words = ["ground", "control", "to", "major", "tom"];
+ 
 
-  const firstNum = function(numArr) {
-    return numArr[0];
-  }
-  
-//   const words = ["ground", "control", "to", "major", "tom"];
-  const nums = [1447, 12357, 7532, 6899, 3457924];
-// const results4 = assertArraysEqual(map(words.length));
-// console.log(results4);
+assertArraysEqual(map(words, word => word[0]), ["g","c","t","m","t"]);
+assertArraysEqual(map([1, 2, 3], num => num[0]), [1, 2, 3]);
+assertArraysEqual(map(["cat", "dog", "squirrel"], word => word[0], ["d", "k", "c"]));
 
-// console.log(assertArraysEqual(map(words, firstLetter)));
-// console.log(assertArraysEqual(map(words, getLength)));
-// console.log(assertArraysEqual(map2(nums, firstNum)));
-console.log(assertArraysEqual(map(words, getLength)), [6, 7, 2, 5, 3]);
-console.log(assertArraysEqual(map(nums, firstNum)), [1, 1, 7, 6, 3]);
-console.log(assertArraysEqual(map(words, firstLetter)),['g','c','t','m','t']);
+
+
