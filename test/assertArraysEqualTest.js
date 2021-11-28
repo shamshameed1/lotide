9eead51758df94f5745b56e55352e6a896f2804f
Shams Hameed <shamsbasim@yahoo.com>
2021-11-27 19:20:38 -0500

    assertarraysequaltest

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,7 +1,19 @@
-
-const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 
+describe("#assertArraysEqual", () => {
+  it("returns true for [1, 2, 3] === [1, 2, 3]", () => {
+    assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+  });
 
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertArraysEqual(eqArrays(["3", "4", "5"], ["5", "4", "3"]), false);
+  it("returns false for ['3', '4', '5'], ['5', '4', '3'] ", () => {
+    assert.strictEqual(assertArraysEqual(["3", "4", "5"], ["5", "4", "3"])); 
+  });
+});
+
+
+
+
+
+// assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+// assertArraysEqual(eqArrays(["3", "4", "5"], ["5", "4", "3"]), false);
